31cd7faad3dbc6c6909478cc7b965fc345e330b7
Yweishihao <342450024@qq.com>
2018-03-14 17:34:35 +0800

    又一次修改

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default class App extends Component<Props> {
   render() {
 
 
-    return (1233
+    return (123345899522
 <View style={styles.container}>
       <View style={styles.container}>
 
