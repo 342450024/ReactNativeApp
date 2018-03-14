c3cebb54ea2e3ffe72d2ca635f1733c69e8622e8
Yweishihao <342450024@qq.com>
2018-03-14 16:47:40 +0800

    123

---
+++ HelloComponent.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+export default class HelloComponent extends Component{
+  render() {
+    return (
+        <Text style={{fontSize:20,backgroundColor:'red'}}>
+          Welcome!
+        </Text>1315222254446
+    );
+  }
+}
