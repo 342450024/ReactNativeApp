131d6dbd10301594f5fde154b088928bdbebd7e4
Yweishihao <342450024@qq.com>
2018-05-14 16:11:47 +0800

    打包完成

--- android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -33,7 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
+          new SplashScreenReactPackage(),
           new DplusReactPackage()
       );
     }
